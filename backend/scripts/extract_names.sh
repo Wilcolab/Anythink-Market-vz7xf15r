77265dbe9d7247087f5ee0c45f9030743379b23c
AlekseiKastsiuk <97795074+AlekseiKastsiuk@users.noreply.github.com>
2023-03-08 20:24:19 +0300

    extract_names.sh

---
+++ backend/scripts/extract_names.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+echo -n>result.txt
+wrong="@amazon.com"
+while IFS="," read -r id lastname firstname email price country
+do
+ if [[ ${email,,} == *"$wrong"* ]]
+ then
+  echo "$firstname $lastname" >> result.txt
+ fi
+done < <(tail -n +2 $1)
